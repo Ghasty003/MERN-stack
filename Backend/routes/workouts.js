3fca7c5ddd95c380d29f0a0fbb3f7b8a0279dd43
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-29 18:19:49 -0400

    Updated

--- Backend/routes/workouts.js
+++ Backend/routes/workouts.js
@@ -7,7 +7,11 @@ const {
   deleteWorkOut,
 } = require("../controller/workoutController");
 
+const requireAuth = require("../middleware/requireAuth");
+
 const router = express.Router();
+
+router.use(requireAuth);
 
 router.get("/", getWorkouts);
 
