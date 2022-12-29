328484206227a44f9192a3e565e4e545c4dec536
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-29 16:12:00 -0400

    Updated

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,6 +7,12 @@ function NavBar() {
             <div className="container">
                 <Link to="/"><h1>Workout Buddy</h1></Link>
             </div>
+            <nav>
+                <div>
+                    <Link to="/login">Login</Link>
+                    <Link to="/signup">Signup</Link>
+                </div>
+            </nav>
         </header>
     );
 }
