838ce6f5ceca0e8cc508024924d9eab282fb8f87
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-29 19:03:33 -0400

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { useContext } from "react";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import NavBar from "./components/NavBar";
+import AuthContext from "./context/AuthContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 function App() {
+  
+  const { user } = useContext(AuthContext);
 
   return (
     <div className="App">
@@ -12,9 +16,9 @@ function App() {
         <NavBar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+            <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
           </Routes>
         </div>
       </BrowserRouter>
