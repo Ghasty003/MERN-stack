65bcfb4f8ced63b7b444ad2b778b2a4ed118785d
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-29 17:07:02 -0400

    Updated

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useLogout from '../hook/useLogout';
 
 function NavBar() {
+    const { logout } = useLogout();
+
+    const handleClick = () => {
+        logout();
+    }
+
     return (
         <header>
             <div className="container">
                 <Link to="/"><h1>Workout Buddy</h1></Link>
                 <nav>
+                    <div>
+                        <button onClick={handleClick}>Logout</button>
+                    </div>
                     <div>
                         <Link to="/login">Login</Link>
                         <Link to="/signup">Signup</Link>
